b123d1d5aeee81ce495032a1ebc1cc94c4aeba7e
Andrew Segre <asegre18@cmc.edu>
2020-08-19 17:47:33 -0700

    getting there

--- develop/script.js
+++ develop/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-    var APIkey = '166a433c57516f51dfab1f7edaed8413';
+    var APIkey = 'd11771b7ba65e99baa089a03cbd1362b';
     // var cityChoice = prompt('what city do you want to know the weather in?');
     // var countryChoice = prompt('what country is it in?').replace(' ', '');
     var cityChoice = "San Francisco";
     var countryChoice = "USA";
     $.ajax({
-      url: `https://api.openweathermap.org/data/2.5/weather?q=${cityChoice},${countryChoice}&appid=${APIkey}`,
+      url: `https://api.openweathermap.org/data/2.5/weather?q=${cityChoice},&units=imperial&appid=${APIkey}`,
       method: "GET"
     }).then(function (response) {
       console.log(response);
@@ -13,9 +13,16 @@ $(document).ready(function () {
       var wind = response.wind.speed;
       var humidity = response.main.humidity;
       var temp = (parseInt(response.main.temp) - 273.15) * 1.80 + 32;
-    //   $('.city').append('<h1>').text(`City: ${city}`).css("font-size", 'xxx-large');
-    //   $('.wind').append('<p>').text(`Wind Speed: ${wind} m/h`);
-    //   $('.humidity').append('<p>').text(`Humidity: ${humidity}%`);
-    //   $('.temp').append('<p>').text(`Temperature: ${temp} F`);
+      $('.city').append('<h1>').text(`City: ${city}`).css("font-size", 'xxx-large');
+      $('.wind').append('<p>').text(`Wind Speed: ${wind} m/h`);
+      $('.humidity').append('<p>').text(`Humidity: ${humidity}%`);
+      $('.temp').append('<p>').text(`Temperature: ${temp} F`);
     });
+
+    let nameOfCity = $(".city-name");
+    for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i);
+        let value = localStorage.getItem(i);
+        localStorOutput.innerHTML += `${key}: ${value}<br />`;
+    }
 });
